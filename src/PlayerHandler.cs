d5b20bb63ed65b53d5b7bb4ef5ed026160bce677
WWYDF <spqrrowplayz@gmail.com>
2024-10-09 06:44:52 -0400

    rewrite
    - Restructured a lot of the mod.
    - All basic features function as both a host and a client.
    - No known crashes.

--- src/PlayerHandler.cs
+++ src/PlayerHandler.cs
@@ -3,29 +3,27 @@ using Dissonance;
 using HarmonyLib;
 using Player;
 using ProximityChat;
-using ProximityChat.DissonanceUtils;
+using ProximityChat.Dissonance;
+using ProximityChat.SteamComms;
 using SNetwork;
 
 namespace ProximityChat.PlayerHandler
 {
     public class SlotManager
     {
+        // Giver Instance Loader.
         private static SlotManager _instance;
-
-        // Public property to access the singleton instance
         public static SlotManager Instance
         {
             get
             {
                 if (_instance == null)
-                {
                     _instance = new SlotManager();
-                }
                 return _instance;
             }
         }
 
-        Dictionary<int, PlayerAgent> playerAgentsBySlot = new Dictionary<int, PlayerAgent>();
+        public Dictionary<int, PlayerAgent> playerAgentsBySlot = new Dictionary<int, PlayerAgent>();
 
         public void UpdatePlayerSlot(int slotNumber, PlayerAgent playerAgent)
         {
@@ -82,6 +80,74 @@ namespace ProximityChat.PlayerHandler
 
             MainPlugin.SendLog.LogWarning($"[PlayerHandler] PlayerAgent {playerAgent.PlayerName} not found in any slot.");
             return null;  // Return null if the PlayerAgent is not found in any slot
+        }
+    }
+
+    public class PlayerManager
+    {
+        // Setup instance linking.
+        private MainPlugin rootInstance;
+        private DissonanceUtils dissonanceInstance;
+        private SteamLink steamLink;
+        private SlotManager slotManager;
+
+        // Giver Instance Loader.
+        private static PlayerManager _instance;
+        public static PlayerManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PlayerManager();
+                return _instance;
+            }
+        }
+
+        public void Init()
+        {
+            // Grab instances.
+            rootInstance = MainPlugin.Instance;
+            dissonanceInstance = DissonanceUtils.Instance;
+            slotManager = SlotManager.Instance;
+            steamLink = SteamLink.Instance;
+        }
+
+        public void RefreshDictionary()
+        {
+            if (slotManager.playerAgentsBySlot != null)
+                slotManager.ClearAllSlots();
+
+            foreach (var player in SNet.LobbyPlayers)
+            {
+                int pSlot = player.CharacterIndex;
+                PlayerAgent agent = null;
+                bool tryGetPlayerAgent = Player.PlayerManager.TryGetPlayerAgent(ref pSlot, out agent);
+
+                if (agent == null || !tryGetPlayerAgent)
+                {
+                    MainPlugin.SendLog.LogError($"Player '{player.NickName}' doesn't have a PlayerAgent Object!");
+                    continue;
+                }
+
+                slotManager.UpdatePlayerSlot(pSlot, agent);
+                MainPlugin.SendLog.LogInfo($"Added {agent.PlayerName} in slot #{pSlot} to Dictionary!");
+            }
+        }
+
+        public void RefreshPlayers() // Runs when a new player joins, or just needs to refresh logic.
+        {
+            if (!dissonanceInstance.isInLevel)
+                return;
+
+            MainPlugin.SendLog.LogInfo("[PlayerManager] Refreshing...");
+            RefreshDictionary(); // wipe/load slot dictionary
+
+            rootInstance.UpdateGlobalDS();
+
+            if (SNet.LocalPlayer.IsMaster)
+                steamLink.HostSteamP2P();
+            else
+                steamLink.ClientSteamP2P();
         }
     }
 }
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,43 +1,74 @@
 ﻿using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using BepInEx;
-using GTFO.API;
-using SNetwork;
-using Player;
-using ProximityChat.PlayerHandler;
-using ProximityChat.DissonanceUtils;
-using TenChambers.EventHandlers;
 using HarmonyLib;
+using ProximityChat.PlayerHandler;
+using ProximityChat.SteamComms;
+using ProximityChat.Dissonance;
+using UnityEngine;
 
 namespace ProximityChat
 {
     [BepInPlugin("net.devante.gtfo.proximitychat", "ProximityChat", "1.0.0")]
     public class MainPlugin : BasePlugin
     {
+        // Setup instance linking.
+        private Dissonance.DissonanceUtils? dissonanceInstance;
+        private PlayerHandler.SlotManager? slotManagerInstance;
+        private SteamComms.SteamLink? steamLink;
+        private PlayerHandler.PlayerManager? playerManager;
+
+        // Giver Instance Loader
+        private static MainPlugin _instance;
+
+        public static MainPlugin Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new MainPlugin ();
+                return _instance;
+            }
+        }
+
+        // Proceed.
+
         public static ManualLogSource SendLog;
-        public bool isPlayerInLevel = false;
-        private PlayerHandler.SlotManager? slotManager;
-        private DissonanceUtils.DissonanceUtils dissonanceUtils;
+        public GameObject? globalDS;
 
         public override void Load() // Runs once when plugin is loaded.
         {
-            // Assign BepInEx logger to static field
+        // Grab instances.
+            dissonanceInstance = DissonanceUtils.Instance;
+            slotManagerInstance = SlotManager.Instance;
+            steamLink = SteamLink.Instance;
+            playerManager = PlayerHandler.PlayerManager.Instance;
+
+        // Assign BepInEx logger to static field
             SendLog = Log;
-            slotManager = SlotManager.Instance;
-            dissonanceUtils = DissonanceUtils.DissonanceUtils.Dissinstance;
 
-            // dissonanceUtils.ReportGSM();
+        // Initialize other classes.
+            dissonanceInstance.Init();
+            steamLink.Init();
+            playerManager.Init();
 
-            LevelAPI.OnEnterLevel += EnterLevel;
-            SendLog.LogInfo("Loaded plugin and utilities!");
-
+        // Patch Harmony.
             var harmony = new Harmony("net.devante.gtfo.proximitychat");
             harmony.PatchAll();
+
+        // Report Finished.
+            SendLog.LogInfo("Loaded plugin and utilities!");
         }
 
-        public void EnterLevel()
+        public void UpdateGlobalDS() // Grabs Main Dissonance GameObject.
         {
-            dissonanceUtils.Init();
+            GameObject globalObj = GameObject.Find("GLOBAL");
+            SendLog.LogInfo($"[UpdateDS] Found GLOBAL GameObject: {globalObj.name}");
+
+            GameObject DSObj = globalObj.transform.Find("Managers/Chat/DissonanceSetup").gameObject;
+            SendLog.LogInfo($"[UpdateDS] Found DissonanceSetup GameObject: {DSObj.name}");
+
+            globalDS = DSObj; // Updates globalDS.
         }
     }
 }
